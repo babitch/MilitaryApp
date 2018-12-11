765e0ef4a93ed0f2f6ad9507e83cfe7091959c2b
Denis Babichev <denis.babichev@leroymerlin.ru>
2018-12-11 11:58:18 +0300

    added new images for main scheme

--- obj/x86/Debug/MillitaryApp_Content.g.i.cs
+++ obj/x86/Debug/MillitaryApp_Content.g.i.cs
@@ -8,9 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("%d0%a4%d0%be%d1%82%d0%be%d0%b3%d1%80%d0%b0%d1%84%d0%b8%d0%b8%20%d0%b0%d0%bf%d0%bf" +
-    "%d0%b0%d1%80%d0%b0%d1%82%d1%83%d1%80%d1%8b/1.jpg")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("%d0%a4%d0%be%d1%82%d0%be%d0%b3%d1%80%d0%b0%d1%84%d0%b8%d0%b8%20%d0%b0%d0%bf%d0%bf" +
-    "%d0%b0%d1%80%d0%b0%d1%82%d1%83%d1%80%d1%8b/2.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("%d0%a1%d0%ba%d1%80%d0%b8%d0%bd%d1%8b%20%d0%be%d1%81%d0%bd%d0%be%d0%b2%d0%bd%d0%be" +
+    "%d0%b9%20%d1%81%d1%85%d0%b5%d0%bc%d1%8b/%d0%91%d0%9a%d0%9a%d0%a0.jpg")]
 
 
